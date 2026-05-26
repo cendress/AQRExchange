cc3461724e63d65a19b88274b7240b3d7b55dfab
cendress <95492688+cendress@users.noreply.github.com>
2026-05-26 14:02:52 -0400

    Build the ExchangeRate model

--- AQRExchange/Models/ExchangeRate.swift
+++ AQRExchange/Models/ExchangeRate.swift
@@ -5,8 +5,26 @@
 //  Created by Christopher Endress on 5/26/26.
 //
 
-import Foundation
+import SwiftUI
 
 struct ExchangeRate: Codable {
+    let ask: String
+    let bid: String
+    let book: String
+    let date: String
     
+    var askDecimal: Decimal? {
+        Decimal(string: ask)
+    }
+    
+    var bidDecimal: Decimal? {
+        Decimal(string: bid)
+    }
+    
+    var currency: String? {
+        book
+            .split(separator: "_")
+            .last
+            .map { String($0).uppercased() }
+    }
 }

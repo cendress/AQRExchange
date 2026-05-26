b2f6b214e1cf9c1bd26083d3469e06e6d62d4c4f
cendress <95492688+cendress@users.noreply.github.com>
2026-05-26 13:39:37 -0400

    Initial Commit

---
+++ AQRExchange/AQRExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AQRExchangeApp.swift
+//  AQRExchange
+//
+//  Created by Christopher Endress on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct AQRExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

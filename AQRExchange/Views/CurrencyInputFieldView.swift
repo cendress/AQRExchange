00f3cba9cd84cc3d46ebaa5f41c03ddf23af56a4
cendress <95492688+cendress@users.noreply.github.com>
2026-05-27 15:01:53 -0400

    Create CurrencyInputFieldView

---
+++ AQRExchange/Views/CurrencyInputFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyInputFieldView.swift
+//  AQRExchange
+//
+//  Created by Christopher Endress on 5/27/26.
+//
+
+import SwiftUI
+
+struct CurrencyInputFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrencyInputFieldView()
+}
